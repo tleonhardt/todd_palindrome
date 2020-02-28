456b68f990d606f5e80e3c5668c0169a4ed23068
Todd Leonhardt <todd.leonhardt@gmail.com>
2020-02-28 02:29:38 +0000

    Added a test

--- test/todd_palindrome_test.rb
+++ test/todd_palindrome_test.rb
@@ -6,10 +6,18 @@ class ToddPalindromeTest < Minitest::Test
   end
 
   def test_non_palindrome
-    assert !"apple".palindrome?
+    refute "apple".palindrome?
   end
 
   def test_literal_palindrome
     assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "Racecar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    skip
   end
 end
