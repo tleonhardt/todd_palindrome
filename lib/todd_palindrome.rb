cc2f26fb6249babcb459436dad6ec575b1df189a
Todd Leonhardt <todd.leonhardt@gmail.com>
2020-02-28 01:26:47 +0000

    Initialize repository

---
+++ lib/todd_palindrome.rb
@@ -0,0 +1,6 @@
+require "todd_palindrome/version"
+
+module ToddPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/todd_palindrome/version.rb
@@ -0,0 +1,3 @@
+module ToddPalindrome
+  VERSION = "0.1.0"
+end
